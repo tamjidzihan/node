1f12acc3b602c982f3790510a80da16d04635690
tamjidzihan <tamjidzihan@gmail.com>
2024-08-13 15:29:22 +0600

    office 2024-08-13

---
+++ app.js
@@ -0,0 +1,9 @@
+const logger = require('./logger')
+
+
+
+const sayHello = (name) => {
+    console.log(`Hello ${name}`)
+}
+
+logger.loggerInfo('hello world')
---
+++ logger.js
@@ -0,0 +1,5 @@
+const loggerInfo = (message) => {
+    console.log(message);
+}
+
+module.exports.loggerInfo = loggerInfo
